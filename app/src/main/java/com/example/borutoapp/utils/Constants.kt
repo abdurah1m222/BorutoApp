f20bffdf03af5b4a781a366c1262a4dd56dfb419
Abdurah1m <abdy42796@gmail.com>
2022-12-17 17:22:48 +0600

    WelcomeScreen

--- app/src/main/java/com/example/borutoapp/utils/Constants.kt
+++ app/src/main/java/com/example/borutoapp/utils/Constants.kt
@@ -7,4 +7,6 @@ object Constants {
     const val HERO_REMOTE_KEY_DATABASE_TABLE = "hero_remote_key_table"
     const val BORUTO_DATABASE = "boruto_app_database"
 
+    const val ON_BOARDING_PAGE_COUNT = 3
+
 }
