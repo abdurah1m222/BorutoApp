9b4e23ea0efd044222220165ad4d8f79207a482a
Abdurah1m <abdy42796@gmail.com>
2022-12-20 10:33:29 +0600

    Handle On Boarding Pages && Navigation

--- app/src/main/java/com/example/borutoapp/domain/model/OnBoardingPage.kt
+++ app/src/main/java/com/example/borutoapp/domain/model/OnBoardingPage.kt
@@ -23,7 +23,7 @@ sealed class OnBoardingPage(
     )
     object Third: OnBoardingPage(
         image = R.drawable.power,
-        title = "Explore",
+        title = "Power",
         description = "Check out your hero's power and see how much are they strong comparing to others."
     )
 }
--- app/src/main/java/com/example/borutoapp/navigation/NavGraph.kt
+++ app/src/main/java/com/example/borutoapp/navigation/NavGraph.kt
@@ -7,18 +7,19 @@ import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import com.example.borutoapp.presentation.screens.home.HomeScreen
 import com.example.borutoapp.presentation.screens.splash.SplashScreen
 import com.example.borutoapp.presentation.screens.welcome.WelcomeScreen
 import com.example.borutoapp.utils.Constants.DETAILS_ARGUMENT_KEY
 import com.google.accompanist.pager.ExperimentalPagerApi
 
-@OptIn(ExperimentalAnimationApi::class)
+@ExperimentalAnimationApi
 @ExperimentalPagerApi
 @Composable
 fun SetupNavGraph(navController: NavHostController) {
     NavHost(
         navController = navController,
-        startDestination = Screen.Welcome.route
+        startDestination = Screen.Splash.route
         ) {
         composable(route = Screen.Splash.route) {
             SplashScreen(navController = navController)
@@ -26,7 +27,9 @@ fun SetupNavGraph(navController: NavHostController) {
         composable(route = Screen.Welcome.route) {
             WelcomeScreen(navController = navController)
         }
-        composable(route = Screen.Home.route) {}
+        composable(route = Screen.Home.route) {
+            HomeScreen()
+        }
         composable(
             route = Screen.Details.route,
             arguments = listOf(navArgument(DETAILS_ARGUMENT_KEY){
